6e19195e9357ffaeb38a923102e9532b3e484229
fr0zensn0w <liquidsn0w@lawn-143-215-53-133.lawn.gatech.edu>
2017-04-15 19:45:04 -0400

    Added geolib module to check for a point in a radius
    Added geolib module to check for a picture’s GPS coordinates in a
    radius defined from the map.

--- Epix UI 4.0/slideshow.js
+++ Epix UI 4.0/slideshow.js
@@ -2,6 +2,19 @@ const remote = require('electron').remote
 const main = remote.require('./main.js')
 const {shell} = require('electron')
 const fs = require('fs')
+const geo = require('geolib')
+
+
+// function to see if a point is inside a given circle given
+// center point and radius
+function calculateInCircle(newLat, newLong, lat, long, radius) {
+  return geo.isPointInCircle(
+    {latitude: 51.525, longitude: 7.4575},
+    {latitude: 51.5175, longitude: 7.4678},
+    5000
+  );
+}
+
 
 function openNewSlideshow() {
     // DO NOT DELETE - handles opening the page
