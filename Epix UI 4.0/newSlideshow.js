f4c58acb9c9353138dbc28f26bd9c3d34776ea81
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-04-17 00:27:22 -0400

    NewSlideshow adds to database

--- Epix UI 4.0/newSlideshow.js
+++ Epix UI 4.0/newSlideshow.js
@@ -7,6 +7,7 @@ const remote = require('electron').remote
 const main = remote.require('./main.js')
 const fs = require('fs')
 const path = require('path')
+const sql = require('sql.js')
 
 
 
@@ -41,41 +42,93 @@ function openSlideshows() {
 }
 
 function saveSlideshowSettings() {
-    var tag = " "
-    var name = document.getElementById('name').value
-    var month = " "
-    var location = " "
-    var exif = " "
-    
-    if (document.getElementById('month-checkbox').checked) {
-        month = document.getElementById('month').value
+    var inBuff = fs.readFileSync('./database.sqlite')
+    console.log("database found!")
+    var db = new sql.Database(inBuff)
+        
+
+    var today = $('#day-checkbox').prop('checked')
+    if(today) {
+        console.log('today')
     }
-    if (document.getElementById('location-checkbox').checked) {
-        location = document.getElementById('location').value
-    }
-    if (document.getElementById('tags-checkbox').checked) {
-        tag += document.getElementById('tag').value
-    }
-    if (document.getElementById('exif-checkbox').checked) {
-        exif = document.getElementById('exif').value
-    }
-    var ssObj = new Object()
-    var json = []
-    ssObj.Name = name
-    ssObj.Tag = tag
-    ssObj.Month = month
-    ssObj.Location = location
-    ssObj.Exif = exif
-    fs.stat('./settings.json', function(err, stat) {
-        if (err == null) {
-            fs.readFile('./settings.json', function(err, data) {
-                json = JSON.parse(data);
-                json.push(ssObj);
-                fs.writeFileSync('settings.json', JSON.stringify(json, null, 4));
-            })
-        } else {
-            json.push(ssObj)
-            fs.writeFileSync('settings.json', JSON.stringify(json, null, 4));
+    var tags = $('#tag').val()
+
+    if (tags) {
+        var splitTags = tags.split(',')
+        for (i = 0; i < splitTags.length; i++){
+            splitTags[i] = splitTags[i].trim()
         }
-    });
+    }
+
+    var name = $('#name').val()
+    console.log(name)
+    var month = null
+    if (!today) {
+        month = $('#month').text()
+        console.log(month)
+    }
+    var year = null
+    var lat = null
+    var lon = null
+    var rad = null
+    var coordinates = remote.getGlobal('sharedObj').coordinates
+    if (location.length < 4) {
+        console.log(location)
+        lat = location[0]
+        lon = location[1]
+        rad = location[2]
+    }
+    var make = $('#make-picker option:selected').text()
+    console.log(make)
+    var model = $('#model-picker option:selected').text()
+    var exp = null
+
+
+    try {
+        var query = "INSERT INTO Slideshow VALUES ('" +
+            name + "'," +
+            (model ? ("'" + model + "'") : 'NULL') + "," +
+            (make ? ("'" + make + "'") : 'NULL') + "," +
+            "NULL,NULL,NULL,NULL,NULL,NULL,NULL," +
+            (lat ? lat : 'NULL') + "," +
+            (lon ? lon : 'NULL') + "," +
+            (rad ? rad : 'NULL') + ");"
+        db.run(query)
+
+        var tagQuery = ""
+        for (i = 0; i < splitTags.length; i++) {
+            tagQuery += "INSERT INTO SlideshowTags VALUES ('" +
+                name + "','" + splitTags[i] + "');"
+        }
+        db.run(tagQuery)
+        console.log(db.exec("SELECT * FROM SlideshowTags"))
+
+    } catch (e) {
+        console.log(e)
+    }
+
+    
+    var dbBinary = db.export()
+    var buff = new Buffer(dbBinary)
+    fs.writeFileSync("database.sqlite", buff)
+    // console.log(db.exec("SELECT * FROM Slideshow;"))
+    db.close()
+    // var dateStart = document.getElementById('date-start')
+    // var dateEnd = document.getElementById('date-end')
+    
+
+    
+    // we probably don't need this anymore
+    // fs.stat('./settings.json', function(err, stat) {
+    //     if (err == null) {
+    //         fs.readFile('./settings.json', function(err, data) {
+    //             json = JSON.parse(data);
+    //             json.push(ssObj);
+    //             fs.writeFileSync('settings.json', JSON.stringify(json, null, 4));
+    //         })
+    //     } else {
+    //         json.push(ssObj)
+    //         fs.writeFileSync('settings.json', JSON.stringify(json, null, 4));
+    //     }
+    // });
 }
