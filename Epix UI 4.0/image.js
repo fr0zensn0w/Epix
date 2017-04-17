37c0c3cb2ca831bb1d37831208763ee88c35bece
lanahjose <lanahmarie95@gmail.com>
2017-04-16 23:01:50 -0400

    Reverting Add Tags page to what it was
    fixed it

--- Epix UI 4.0/image.js
+++ Epix UI 4.0/image.js
@@ -36,23 +36,92 @@ window.onload = function renderImage() {
     var imgArr = fn.split("/")
     var imgName = (imgArr[imgArr.length-1]).replace("%20", " ")
 
-    console.log(imgName)
-    var img = document.createElement('img');
-    img.src = fn;
-    img.style.width = '600px'
-    img.style.height = '500px'
-    document.body.appendChild(img);
-    var form = document.createElement('form')
-    var input = document.createElement('input')
-    input.type = "text"
-    input.id = "tag"
-    form.appendChild(input);
+    //put everything into this
+    var container = document.getElementById("page-container")
+
+    //img name
+    var imageName = document.createElement('h1')
+    imageName.className = "page-header"
+    imageName.textContent = imgName
+    var imageNameDiv = document.createElement('div')
+    imageNameDiv.className = "row"
+
+    var imageNameCol = document.createElement('div')
+    imageNameCol.className = "col-lg-12"
+    imageNameCol.appendChild(imageName)
+
+    imageNameDiv.appendChild(imageNameCol)
+
+    //back button
+    var backDiv = document.createElement('div')
+    backDiv.id = "back-div"
+
     var backButton = document.createElement('button')
     backButton.textContent = 'Back'
+    backButton.type = "button"
+    backButton.className = "btn btn-sm btn-primary"
     backButton.addEventListener('click', function() {
         openGallery()
     });
+
+    backDiv.appendChild(backButton)
+
+    container.appendChild(backDiv);
+
+    
+
+
+    //div to hold image and tag input row
+    var imageAndInput = document.createElement('div')
+    imageAndInput.id = "image-and-input"
+
+    console.log(imgName)
+    var img = document.createElement('img');
+    img.id = 'image-display'
+    img.src = fn;
+    img.style.width = '600px'
+    img.style.height = '500px'
+
+    imageAndInput.appendChild(img);
+    imageAndInput.appendChild(imageNameDiv);
+
+
+    //input and submit button row and columns
+    var inputRow = document.createElement('div')
+    inputRow.className = "form-group row"
+    inputRow.id = "tag-input"
+
+    var formCol = document.createElement('div')
+    formCol.className = "col-3"
+
+    var buttonCol = document.createElement('div')
+    buttonCol.className = "col-1"
+
+    // document.body.appendChild(img);
+
+
+    //input
+    var form = document.createElement('form')
+    var input = document.createElement('input')
+    input.type = "text"
+    input.className = "form-control"
+    input.value = "add tags"
+    input.id = "tag"
+    form.appendChild(input);
+    // var backButton = document.createElement('button')
+    // backButton.textContent = 'Back'
+    // backButton.addEventListener('click', function() {
+    //     openGallery()
+    // });
+
+
+
+    //submit button
     var button = document.createElement('button')
+
+    button.type = "button"
+    button.className = "btn btn-sm btn-primary"
+
     button.textContent = 'Submit Tag'
     button.addEventListener('click', function() {
         var tag = document.getElementById("tag").value
@@ -78,7 +147,19 @@ window.onload = function renderImage() {
         console.log(db.exec("SELECT * FROM ImageTags WHERE imageName='" + imgName +"';"))
         db.close()
     })
-    document.body.appendChild(form);
-    document.body.appendChild(button);
-    document.body.appendChild(backButton);
+    // document.body.appendChild(form);
+    // document.body.appendChild(button);
+    // document.body.appendChild(backButton);
+
+
+    formCol.appendChild(form);
+    buttonCol.appendChild(button);
+
+    inputRow.appendChild(formCol);
+    inputRow.appendChild(buttonCol);
+
+    imageAndInput.appendChild(inputRow);
+
+
+    container.appendChild(imageAndInput);
 }
