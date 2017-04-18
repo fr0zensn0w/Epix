16e2b9dc1fb436f57843afa06775d84b1d619e6c
Anthony Bonitatibus <abonitatibus3@gatech.edu>
2017-04-18 04:20:06 -0400

    Gallery now populates from database, not old json
    It seems we forgot to fix this

--- Epix UI 4.0/gallery.js
+++ Epix UI 4.0/gallery.js
@@ -58,23 +58,27 @@ function openSelected(i) {
 
 // this function should load images into the frames for the slideshows
 window.onload = function populateImages() {
-    fs.readFile('./data.json', function(err, data) {
-        var imgData = JSON.parse(data)
-        var gallery = document.getElementById("gallery-cards")
-        //var img = document.getElementsByClassName('card-img-top img-fluid w-100')
-        // right now we're just looping through and assigning images in order.
-        // wow actually I just figured out how to make the gallery
-        for (i = 0; i < imgData.length; i++) {
-            var div = document.createElement('div')
-            div.className = "card card-inverse"
-            var img = document.createElement('img')
-            img.className = "card-img-top img-fluid w-100"
-            img.src = `file://${__dirname}/Photos/` + imgData[i].FileName
-            // img.style.height = '250px'
-            // img.style.width = '350px'
+    var inBuff = fs.readFileSync('./database.sqlite')
+    // console.log("database found!")
+    var db = new sql.Database(inBuff)
 
-            img.style.width = imgData[i].Width/20
-            img.style.height = imgData[i].Height/20
+    var query = "SELECT imageName, Width, Height FROM Image;"
+    var imageData = db.exec(query)
+    var imageData = imageData[0].values
+
+    // console.log(imageData)
+
+    var gallery = document.getElementById("gallery-cards")
+
+    for (i = 0; i < imageData.length; i++) {
+
+        var div = document.createElement('div')
+        div.className = "card card-inverse"
+        var img = document.createElement('img')
+        img.className = "card-img-top img-fluid w-100"
+        img.src = `file://${__dirname}/Photos/` + imageData[i][0]
+        img.style.width = imageData[i][1]/20
+            img.style.height = imageData[i][2]/20
 
             img.addEventListener('click', function(e) {
                 //openSelected(e);
@@ -83,10 +87,12 @@ window.onload = function populateImages() {
 
             div.appendChild(img);
             gallery.appendChild(div);
-        }
-    })
-}
 
-// <div class="card">
-//                 <img class="card-img-top img-fluid w-100" src="http://placehold.it/350x150" alt="Card image cap">
-//             </div>
+    }
+
+
+    var dbBinary = db.export()
+    var buff = new Buffer(dbBinary)
+    fs.writeFileSync("database.sqlite", buff)
+    db.close() 
+}
