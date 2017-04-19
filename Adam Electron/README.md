29a4499be34c17a166b4c8e17f05c5db95779672
fr0zensn0w <liquidsn0w@lawn-143-215-53-133.lawn.gatech.edu>
2017-04-19 17:44:18 -0400

    Adding readme and removing unnecessary files
    Adding readme and removing unnecessary files

--- Adam Electron/README.md
+++
@@ -1,2 +0,0 @@
-Debugging package on electron https://github.com/sindresorhus/electron-debug
-has all the pertinent commands
--- Adam Electron/slideshowHandler.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var d3 = require('d3');
-var ExifImage = require('exif').ExifImage;
-var path = require('path');
-var process = require('process');
-
-var appDirectory = __dirname + "/Photos";
